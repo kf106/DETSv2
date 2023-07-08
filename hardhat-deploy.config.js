eff98540bbf3d7fc5fb91b3ae6c1bcde71e77949
Keir Finlow-Bates <keir@chainfrog.com>
2023-07-08 16:06:50 +0300

    Update configs some more

--- hardhat-deploy.config.js
+++ hardhat-deploy.config.js
@@ -11,12 +11,10 @@ require("solidity-coverage");
 require('dotenv-safe').config();
 
 const { 
-    RINKEBY_API_URL, 
     MAINNET_API_URL,
     MATIC_API_URL,
     ETHSCAN_API_KEY,
     POLYGONSCAN_API_KEY,
-    RINKEBY_PRIVATE_KEY, 
     MAINNET_PRIVATE_KEY,
     MATIC_PRIVATE_KEY,
     COINMARKETCAP_API_KEY,
@@ -52,13 +50,6 @@ module.exports = {
       url: 'http://127.0.0.1:8555',
       gas: 0xfffffffffff,
       gasPrice: 0x01,
-      base_gas: BASE_GAS,
-      priority_gas: PRIORITY_GAS,
-      multiplier: MAX_FEE_MULTIPLIER,
-    },
-    rinkeby: {
-      url: RINKEBY_API_URL,
-      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
       base_gas: BASE_GAS,
       priority_gas: PRIORITY_GAS,
       multiplier: MAX_FEE_MULTIPLIER,
